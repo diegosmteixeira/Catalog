41bdb1eca14ecb81cbea08852bfe828e8a240834
diegosmteixeira <diegosmteixeira@gmail.com>
2021-06-29 18:37:49 -0300

    Exception Handling

--- APICatalogo/Controllers/CategoriesController.cs
+++ APICatalogo/Controllers/CategoriesController.cs
@@ -1,80 +1,123 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace APICatalogo.Controllers
 {
-        [Route("api/[Controller]")]
-        [ApiController]
-        public class CategoriesController : ControllerBase
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CategoriesController(AppDbContext context)
         {
-            private readonly AppDbContext _context;
-            public CategoriesController(AppDbContext context)
-            {
-                _context = context;
-            }
+            _context = context;
+        }
 
-            [HttpGet("products")]
-            public ActionResult<IEnumerable<Category>> GetCategoriesProducts()
-            {
-                return _context.Categories.Include(x => x.Products).ToList();
-            }
+        [HttpGet("products")]
+        public ActionResult<IEnumerable<Category>> GetCategoriesProducts()
+        {
+            return _context.Categories.Include(x => x.Products).ToList();
+        }
 
-            [HttpGet]
-            public ActionResult<IEnumerable<Category>> Get()
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> Get()
+        {
+            try
             {
                 return _context.Categories.AsNoTracking().ToList();
-            }
 
-            [HttpGet("{id}", Name = "GetCategory")]
-            public ActionResult<Category> Get(int id)
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Database categories access error.");
+            }
+        }
+
+        [HttpGet("{id}", Name = "GetCategory")]
+        public ActionResult<Category> Get(int id)
+        {
+            try
             {
                 var category = _context.Categories.AsNoTracking()
                     .FirstOrDefault(p => p.CategoryId == id);
 
                 if (category == null)
                 {
-                    return NotFound();
+                    return NotFound($"Category id: {id} was not found.");
                 }
                 return category;
             }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Database categories access error.");
+            }
+        }
 
-            [HttpPost]
-            public ActionResult Post([FromBody] Category category)
+        [HttpPost]
+        public ActionResult Post([FromBody] Category category)
+        {
+            try
             {
                 _context.Categories.Add(category);
                 _context.SaveChanges();
 
                 return new CreatedAtRouteResult("GetCategory", new { id = category.CategoryId }, category);
             }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error to create new category.");
+            }
+        }
 
-            [HttpPut("{id}")]
-            public ActionResult Put(int id, [FromBody] Category category)
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Category category)
+        {
+            try
             {
                 if (id != category.CategoryId)
                 {
-                    return BadRequest();
+                    return BadRequest($"Could not possible save changes to category id: {id}.");
                 }
                 _context.Entry(category).State = EntityState.Modified;
                 _context.SaveChanges();
                 return Ok();
             }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                        $"Error to save changes to category id: {id}.");
+            }
+        }
 
-            [HttpDelete("{id}")]
-            public ActionResult<Category> Delete(int id)
+        [HttpDelete("{id}")]
+        public ActionResult<Category> Delete(int id)
+        {
+            try
             {
                 var category = _context.Categories.FirstOrDefault(p => p.CategoryId == id);
 
                 if (category == null)
                 {
-                    return NotFound();
+                    return NotFound($"Category id: {id} was not found.");
                 }
                 _context.Categories.Remove(category);
                 _context.SaveChanges();
                 return category;
             }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Error to delete category id: {id}.");
+            }
         }
+    }
 }
