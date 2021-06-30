87fb46cd1be4b80326b8ebdd7ced09d2205230af
diegosmteixeira <diegosmteixeira@gmail.com>
2021-06-30 20:27:15 -0300

    Data Annotations

--- APICatalogo/Models/Product.cs
+++ APICatalogo/Models/Product.cs
@@ -12,20 +12,31 @@ namespace APICatalogo.Models
     {
         [Key]
         public int ProductId { get; set; }
-        [Required]
-        [MaxLength(80)]
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(20, ErrorMessage = "Name must be between 5 and 20 characters.",
+            
+            MinimumLength =5)]
         public string Name { get; set; }
+
         [Required]
-        [MaxLength(300)]
+        [StringLength(10, ErrorMessage = "Description maximum length {1} characters.")]
         public string Description { get; set; }
+
         [Required]
+        [Range(1, 10000, ErrorMessage = "Price must be between {1} and {2}.")]
         public decimal Price { get; set; }
+
         [Required]
-        [MaxLength(500)]
+        [StringLength(300, MinimumLength = 10)]
         public string ImageUrl { get; set; }
+
         public float Stock { get; set; }
+
         public DateTime Date { get; set; }
+
         public Category Category { get; set; }
+
         public int CategoryId { get; set; }
     }
 }
